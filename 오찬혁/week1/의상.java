5789888d92981d372ef981c81fa39d3a0b5d439c
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-05-03 17:03:36 +0900

    feat: [오찬혁][260505] 전화번호 목록 제출

---
+++ 오찬혁/week1/의상.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+	public int solution(String[][] clothes) {
+		int answer = 1;
+		HashMap<String, Integer> map = new HashMap<>();
+
+		for(String[] cloth : clothes){
+			String type = cloth[1];
+			map.put(type, map.getOrDefault(type, 0) + 1);
+		}
+
+		for(Map.Entry<String, Integer> entry : map.entrySet()){
+			answer *= entry.getValue() + 1;
+		}
+
+		return answer - 1;
+	}
+}
