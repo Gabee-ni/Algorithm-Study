6a635caa5426a8256e9ed3aee1e5a4df781eb67f
gaeunnlee <gaeunnnleee@gmail.com>
2026-05-04 23:30:05 +0900

    feat: [이가은][260505] 완주하지 못한 선수 제출

---
+++ 이가은/260505_완주하지 못한 선수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        HashMap<String, Integer> pMap = new HashMap<>();
+        
+        for (String p: participant) {
+            pMap.put(p, pMap.getOrDefault(p, 0) + 1);
+        }
+        
+        for (String c: completion) {
+            pMap.put(c, pMap.get(c) - 1);
+            if (pMap.get(c) == 0) {
+                pMap.remove(c);
+            }
+        }        
+        
+        for (String p: pMap.keySet()) {
+            return p;
+        }
+        
+        return answer;
+    }
+}
