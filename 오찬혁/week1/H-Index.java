28886392fe5b5412c76728f0b30d4ba96630e043
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-05-06 18:11:34 +0900

    feat: [오찬혁][260508] H-Index 제출

---
+++ 오찬혁/week1/H-Index.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+	public int solution(int[] citations) {
+		int answer = 0;
+
+		Arrays.sort(citations);
+
+		int left = 0;
+		int right = citations[citations.length - 1];
+
+		while(left <= right){
+			int mid = (left + right) / 2;
+
+			if(canUse(citations, mid)){
+				answer = mid;
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+
+		return answer;
+	}
+
+	boolean canUse(int[] citations, int mid){
+		int cnt = 0;
+		for(int ci : citations){
+			if(ci >= mid){
+				cnt++;
+			}
+		}
+
+		return cnt >= mid;
+	}
+}
