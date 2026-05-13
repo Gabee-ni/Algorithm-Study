94ebef34fa36e6ef18644f56ac7e7109a2cb1b97
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-05-13 21:04:14 +0900

    feat: [오찬혁][260514] 모음사전 제출

---
+++ 오찬혁/week2/모음사전.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+
+	String[] vowels = {"A", "E", "I", "O", "U"};
+	List<String> dict = new ArrayList<>();
+
+	public int solution(String word) {
+
+		dfs("");
+
+		return dict.indexOf(word) + 1;
+	}
+
+	void dfs(String cur){
+		if(cur.length() > 5){
+			return;
+		}
+		if(cur.length() > 0){
+			dict.add(cur);
+		}
+
+		for(int i = 0; i < vowels.length; i++){
+			dfs(cur + vowels[i]);
+		}
+	}
+}
