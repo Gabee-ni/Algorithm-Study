3105aee5a101aa91ce4c28a60cff0257c295cc78
Gabeen-ni <been.dev.kim@gmail.com>
2026-05-18 10:58:41 +0900

    feat: [김가빈][260518] 5월 식품들의 매출 조회 제출

---
+++ 김가빈/week03/260518_5월식품들의총매출조회하기.sql
@@ -0,0 +1,6 @@
+SELECT p.product_id, p.product_name, SUM(p.price * o.amount) AS total_sales
+FROM food_product p
+         JOIN food_order o ON p.product_id = o.product_id
+WHERE DATE_FORMAT(o.produce_date, '%Y-%m') = '2022-05'
+GROUP BY p.product_id,  p.product_name
+ORDER BY total_sales DESC, p.product_id
