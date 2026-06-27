9cb1c3a811443206781e8859ad9aa2aa844920cf
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2026-06-27 19:53:39 +0900

    feat: [오찬혁][260625] 5회차 제출

---
+++ [실전모의코테]/05회차/오찬혁/주차 요금 계산.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+	public int[] solution(int[] fees, String[] records) {
+		Map<String, Integer> inTime = new HashMap<>();
+		Map<String, Integer> total = new TreeMap<>();
+
+		for(String record : records){
+			String[] p = record.split(" ");
+			int time = toMinutes(p[0]);
+			String car = p[1];
+
+			if("IN".equals(p[2])){
+				inTime.put(car, time);
+			} else {
+				total.merge(car, time - inTime.remove(car), Integer::sum);
+			}
+		}
+
+		int endOfDay = 23 * 60 + 59;
+
+		for(var e : inTime.entrySet()){
+			total.merge(e.getKey(), endOfDay - e.getValue(), Integer::sum);
+		}
+
+		int[] result = new int[total.size()];
+		int idx = 0;
+		for(int t : total.values()){
+			result[idx++] = calcFee(fees, t);
+		}
+
+		return result;
+	}
+
+	private int toMinutes(String hhmm){
+		String[] t = hhmm.split(":");
+		return Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
+	}
+
+	private int calcFee(int[] fees, int time){
+		int fee = fees[1];
+		if(time > fees[0])
+			fee += (int) Math.ceil((double)(time - fees[0]) / fees[2]) * fees[3];
+		return fee;
+	}
+}
